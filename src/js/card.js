daabf24642df4fc78435bbc0811765c3697d100d
Julia Paiva <juliapoliver12@gmail.com>
2024-03-26 17:14:32 -0300

    aaaa

--- src/js/card.js
+++ src/js/card.js
@@ -1,8 +1,13 @@
 const card = document.getElementById('card')
 const infoCard = document.getElementById('info_card')
 
-const mostrarInfo = (Informacoes) => {
+const mostrarInfo = () => {
+    infoCard.classList.remove('teste')
+}
+const sumirInfo = () => {
     infoCard.classList.add('teste')
 }
 
 card.addEventListener('click', mostrarInfo)
+
+infoCard.addEventListener('click', sumirInfo)
